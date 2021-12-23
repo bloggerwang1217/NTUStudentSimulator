dc20b960cec32b8c5377ef6d8a0b3c6c0b240c2c
BloggerWang <bloggerwang1217@protonmail.com>
2021-12-23 17:14:55 +0800

    GUI進度：可顯示開始劇情

--- function/beginning.py
+++ function/beginning.py
@@ -1,30 +1,51 @@
 import tkinter as tk
-from PIL import ImageTk, Image 
+from PIL import ImageTk, Image
 
 def press_start_game(start_scene, start_button, window):
     start_scene.destroy()
     start_button.destroy()
     beginning_story(window)
 
+
 def start_game(window):
 
-    f = tk.font.Font(size = 32, family = "lihsianti")
+    f = tk.font.Font(size = 32)
 
-
-    beginning = Image.open("figure/beginning.jpeg")
+    beginning = Image.open("figure/beginning.png")
     beginning = beginning.resize((1280, 720), Image.ANTIALIAS)
     beginning = ImageTk.PhotoImage(beginning)
 
     start_scene = tk.Label(window, image = beginning)
+    start_scene.image = beginning
     start_scene.pack(fill = "both")
 
-    start_button = tk.Button(window, text = "開始遊戲",width = 10, font = f, command = lambda: press_start_game(start_scene, start_button, window))
-    start_button.place(x = 550, y = 300)
+
+    start_button = tk.Button(window, text = "開始遊戲",width = 7, font = f, command = lambda: press_start_game(start_scene, start_button, window))
+    start_button.place(x = 560, y = 300)
+
 
 def beginning_story(window):
     f = tk.font.Font(size = 48, family = "lihsianti")
-    gray = 0
+
     window.configure(bg="white")
 
-    text = tk.Label(window, text = "你終於進入了理想中的大學\n台大\n帶著既期待又怕受傷害的心情\n你準備迎向這四年未知的生活...", font = f)
-    text.place(x = 300, y = 100)
+    text = [0, 1]  # 加入開場說明文字或圖片
+    text[0] = tk.Label(window, text = "你終於進入了理想中的大學\n台大\n帶著既期待又怕受傷害的心情\n你準備迎向這四年未知的生活...", font = f)
+    text[1] = tk.Label(window, text = "我為什麼在這裡", font = f)
+    text[0].place(x = 300, y = 100)
+
+    index = [0]
+    next_button = tk.Button(window, text = "下一頁",width = 6, font = f, command = lambda: press_next_button(window, text, next_button, index))
+    next_button.place(x = 1000, y = 500)
+
+    
+def press_next_button(window, text, next_button, index):
+    f = tk.font.Font(size = 32)
+    if index[0] == len(text)-1:
+        text[index[0]].destroy()
+        next_button.destroy()
+        # 呼叫輸入姓名函式
+        return
+    text[index[0]].destroy()
+    text[index[0]+1].place(x = 300, y = 100)
+    index[0] += 1
--- main.py
+++ main.py
@@ -12,37 +12,16 @@ window.title("為美好的台大生活獻上祝福")
 
 bg.start_game(window)
 
-
-# def start_game():
-#     start_scene.destroy()
-#     start_button.destroy()
-#     bg.beginning_story(window)
-
-# title_f = tk.font.Font(size = 84, family = "lihsianti")
-# f = tk.font.Font(size = 32, family = "lihsianti")
-
-# beginning = Image.open("figure/beginning.jpeg")
-# beginning = beginning.resize((1280, 720), Image.ANTIALIAS)
-# beginning = ImageTk.PhotoImage(beginning)
-
-# start_scene = tk.Label(window, image = beginning)
-# start_scene.pack()
-
-
-# start_button = tk.Button(window, text = "開始遊戲",width = 10, font = f, command = start_game)
-# start_button.place(x = 550, y = 300)
-
-
 window.mainloop()
 
+# 輸入姓名、性別
 # data = ip.input_basic_data()
 # name = data[0]
 # sex = data[1]
 # print(name, sex)
 
+# 排時間表
 # selected_class = {"1-1":"A課", "2-2":"B課", "4-3":"C課"}
 # previous_picked_schedule = {'1-1': 'A課', '1-2': '讀A課', '1-3': '社交', '1-4': '健身', '2-1': '社交', '2-2': 'B課', '2-3': '讀A課', '2-4': '休息', '3-1': '讀A課', '3-2': '讀B課', '3-3': '讀A課', '3-4': '休息', '4-1': '讀A課', '4-2': '健身', '4-3': 'C課', '4-4': '社交', '5-1': '休息', '5-2': '約會', '5-3': '社交', '5-4': '休息'}
-
 # picked_schedule = sch.get_new_schedule(selected_class, previous_picked_schedule)
-
 # print(picked_schedule)
