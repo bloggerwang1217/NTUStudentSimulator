07213001dcb021d9aa4510f39b4f3146b432bc49
DukeSCSekuD <96483321+DukeSCSekuD@users.noreply.github.com>
2021-12-26 17:49:29 +0800

    Add files via upload

--- function/結局結算評分值.py
+++ function/結局結算評分值.py
@@ -1,16 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
-plt.rcParams['font.family'] = 'Noto Sans TC'  # 要顯示中文有點複雜
-plt.rcParams['axes.unicode_minus'] = False
+from matplotlib import pyplot
 
-point_table = ['愛情進度', '學業成績', '養生', '聲望']
-points = [70,80,52,100]  # 我隨便預設的值，應該要讀入結尾數值
+class abi():
+    pass
+ability = abi()
+ability.love_progress = 40
+ability.grade = 70
+ability.yang_sheng = 80
+ability.prestige = 90
 
 
-# 長與寬可以再討論
-x = np.arange(len(point_table))
-plt.bar(x, points, color=['red', 'green', 'blue', 'yellow'])
-plt.xticks(x, point_table)
-plt.xlabel('評分值')
-plt.ylabel('分數')
-plt.show()
+
+def score_illu(ability):
+    plt.rcParams['font.family'] = 'MingLiU'  # 要顯示中文有點複雜
+    plt.rcParams['axes.unicode_minus'] = False
+    point_table = ['愛情進度', '學業成績', '養生', '聲望']
+    points = [ability.love_progress,
+              ability.grade,
+              ability.yang_sheng,
+              ability.prestige]  # 我隨便預設的值，應該要讀入結尾數值
+    # 圖形長與寬可以再討論
+    x = np.arange(len(point_table))
+    plt.bar(x, points, color=['red', 'green', 'blue', 'yellow'])
+    plt.xticks(x, point_table)
+    plt.xlabel('評分值')
+    plt.ylabel('分數')
+    plt.savefig(r'C:\Users\周匯森\Desktop\00.png')  # 這裡會選擇存檔路徑與檔名
+    plt.show()
+    
+score_illu(ability)
