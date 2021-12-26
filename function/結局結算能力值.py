686ccb4849d25cc362b050e025ff791a23119395
DukeSCSekuD <96483321+DukeSCSekuD@users.noreply.github.com>
2021-12-27 00:01:05 +0800

    Add files via upload

--- function/結局結算能力值.py
+++ function/結局結算能力值.py
@@ -8,7 +8,7 @@ import os
 path = os.getcwd()
 
 title_f = fm.FontProperties(fname=f"{path}/TaipeiSansTCBeta-Regular.ttf", size = 20)
-f = fm.FontProperties(fname=f"{path}/TaipeiSansTCBeta-Regular.ttf", size = 16)
+f = fm.FontProperties(fname=f"{path}/TaipeiSansTCBeta-Regular.ttf", size = 12)
 
 # plt.rcParams['font.family'] = 'MingLiU'  # 要顯示中文有點複雜
 # plt.rcParams['axes.unicode_minus'] = False
@@ -30,14 +30,20 @@ def abi_illu(abi):
     data_length = 5
     # 將極座標根據資料長度進行等分
     angles = np.linspace(0, 2*np.pi, data_length, endpoint=False)
-    labels = [key for key in results[0].keys()]
+    labels = ["智慧" + ':' + str(ability.wisdom),
+              "魅力" + ':' + str(ability.charm),
+              "體能" + ':' + str(ability.fitness),
+              "社交能力" + ':' + str(ability.social_skill),
+              "健康" + ':' + str(ability.health)]
+    print(labels)
     score = [[v for v in result.values()] for result in results]
+    print(score)
     # 使雷達圖資料封閉
     score_a = np.concatenate((score[0], [score[0][0]]))
     angles = np.concatenate((angles, [angles[0]]))
     labels = np.concatenate((labels, [labels[0]]))
     # 設定圖形的大小
-    fig = plt.figure(figsize=(8, 6), dpi=100)
+    fig = plt.figure(figsize=(6, 6), dpi=100)
     # 新建一個子圖
     ax = plt.subplot(111, polar=True)
     # 繪製雷達圖
@@ -52,6 +58,7 @@ def abi_illu(abi):
     ax.set_rlabel_position(270)
     ax.set_title("能力值", font = title_f)
     # plt.legend(["你"], loc='best')
+    ax.spines['polar'].set_visible(False)
     now = str(datetime.now())
     plt.savefig(r'C:\Users\周匯森\Desktop\000.png')  # 這裡會選擇存檔路徑與檔名
     plt.show()
