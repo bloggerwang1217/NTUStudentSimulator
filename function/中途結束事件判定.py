7144d6773425cf62a1a59492bb99be0d2f11190c
DukeSCSekuD <96483321+DukeSCSekuD@users.noreply.github.com>
2021-12-29 22:52:27 +0800

    Update 中途結束事件判定.py

--- function/中途結束事件判定.py
+++ function/中途結束事件判定.py
@@ -1,40 +1,23 @@
 import random
 
-class ability():
-    pass
-abi = ability()
-abi.wisdom = 99
-abi.social_skill = 75
-abi.love_progress = 42
-abi.luck = 15
-abi.charm = 70
-abi.Money = 0
-abi.health = 5
-pass_or_not = True  # 有沒有通過考試，還未決定
-
-
-
 
 To_New_World_freq, Too_Stupid_freq = True, True
 
 def blow_wind():  # 樓頂吹風
-    global abi
-    if abi.social_skill <= 0 or abi.wisdom <= 0: # 這裡需要參考所有的值，故暫定
+    if status.social_skill <= 0 or status.wisdom <= 0 or status.fitness <= 0 or status.charm <= 0 or status.health <= 0:
         return True
 
 
 def To_New_World():  # 轉生異世界
-    global abi
     prob = random.randrange(1, 101)
-    if abi.luck < 5 and prob < 2 and To_New_World_freq:
+    if status.luck < 5 and prob < 2 and To_New_World_freq:
         return True
-    elif abi.luck < 5 and prob > 1 and To_New_World_freq:  # 一學期只會判定一次
+    elif status.luck < 5 and prob > 1 and To_New_World_freq:  # 一學期只會判定一次
         To_New_World_freq = False
 
 
 def Me_First():  # 明明是我先來的
-    global abi
-    if abi.charm > 90 and love_progress < 40:
+    if status.charm > 90 and love_progress < 40:
         return True
 
 
@@ -45,24 +28,30 @@ def Too_Stupid():  # 被二一
 
 
 def Broke():  # 破產
-    global abi
-    if abi.Money <= 0:  # 尚未決定
+    if status.Money <= -50000:
         return True
 
 
 def Wealth_Freedom():  # 財富自由
-    global abi
-    if abi.Money >= 100000:  # 尚未決定
+    if status.Money >= 1000000:
         return True
 
 
 def IntoDust():  # 火化
-    global abi
-    if abi.health <= 0:
+    if status.health <= 0:
         return True
 
 
-
-print(blow_wind(), To_New_World(), Me_First(), Too_Stupid(),
-      Broke(), Wealth_Freedom(), IntoDust())
+outputList = []
+Incident_List = ['樓頂吹風', '轉生異世界', '明明是我先來的', '被二一', '破產', '財富自由', '火化']
+Mid_End = [blow_wind(), To_New_World(), Me_First(), Too_Stupid(), Broke(), Wealth_Freedom(), IntoDust()]
+for i in range(7):
+    if Mid_End[i] = True:
+        outputList.append(Incident_List[i])
+ 
+if outputList != []:
+    for i in outputList:
+        show_MidEnd_events(data, i)
+else:
+    pass
 
