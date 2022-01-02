a360fb9623091a9e08f6294fb117ef481768d617
DukeSCSekuD <96483321+DukeSCSekuD@users.noreply.github.com>
2022-01-02 14:50:37 +0800

    Update 中途結束事件判定.py

--- function/中途結束事件判定.py
+++ function/中途結束事件判定.py
@@ -1,11 +1,13 @@
 import random
+import function.show_event as show
 
+status = "" 
 
 To_New_World_freq, Too_Stupid_freq = True, True
 
 def blow_wind():  # 樓頂吹風
     global status
-    if status.social_skill <= 0 or status.wisdom <= 0 or status.fitness <= 0 or status.charm <= 0 or status.health <= 0:
+    if status.social<= 0 or status.wisdom <= 0 or status.fitness <= 0 or status.charm <= 0 or status.health <= 0:
         return True
 
 
@@ -47,12 +49,175 @@ def IntoDust():  # 火化
     if status.health <= 0:
         return True
 
+    
+def check_event(data):
+    global status
+    status = data["status"]
+    
+    outputList = []
+    Incident_List = ['中途結束事件：樓頂吹風', '中途結束事件：轉生異世界', '中途結束事件：明明是我先來的', '中途結束事件：被二一', '中途結束事件：破產', '中途結束事件：財富自由', '中途結束事件：火化']
+    Mid_End = [blow_wind(), To_New_World(), Me_First(), Too_Stupid(), Broke(), Wealth_Freedom(), IntoDust()]
+    for i in range(7):
+        if Mid_End[i] = True:
+            outputList.append(Incident_List[i])
+            
+    if len(outputList) > 1:
+        prob = random.randrange(0, len(outputList))
+        outputList = outputList[prob]
+        
+    if data["time"] == "大一上"  and data["previous_event"] == "第一次排行程表":
+        data["previous_event"] == "第二次排行程表"
+        outputList.append('必然事件:健康檢查')
+        # 有必然事件就加入
+    if data["time"] == "大一上"  and data["previous_event"] == "第二次排行程表":
+        data["previous_event"] == "期中考"
+        outputList.append('期中考')
+    if data["time"] == "大一上"  and data["previous_event"] == "期中考":
+        data["previous_event"] == "第一次排行程表"
+        outputList.append('第一次排行程表')
+    if data["time"] == "大一上"  and data["previous_event"] == "第一次排行程表":
+        data["previous_event"] == "第二次排行程表"
+        outputList.append('第二次排行程表')
+    if data["time"] == "大一上"  and data["previous_event"] == "第二次排行程表":
+        data["previous_event"] == "期末考"
+        outputList.append('期末考')
 
-outputList = []
-Incident_List = ['中途結束事件：樓頂吹風', '中途結束事件：轉生異世界', '中途結束事件：明明是我先來的', '中途結束事件：被二一', '中途結束事件：破產', '中途結束事件：財富自由', '中途結束事件：火化']
-Mid_End = [blow_wind(), To_New_World(), Me_First(), Too_Stupid(), Broke(), Wealth_Freedom(), IntoDust()]
-for i in range(7):
-    if Mid_End[i] = True:
-        outputList.append(Incident_List[i])
- 
-process_event(data, outputList)  # 一旦觸發中途結束，將直接結束遊戲
+    if data["time"] == "大一下"  and data["previous_event"] == "期末考":
+        data["previous_event"] == "第一次排行程表"
+        outputList.append('第一次排行程表')
+    if data["time"] == "大一下"  and data["previous_event"] == "第一次排行程表":
+        data["previous_event"] == "第二次排行程表"
+        outputList.append('必然事件:社團')
+        outputList.append('第二次排行程表')
+    if data["time"] == "大一下"  and data["previous_event"] == "第二次排行程表":
+        data["previous_event"] == "期中考"
+        outputList.append('期中考')
+    if data["time"] == "大一下"  and data["previous_event"] == "期中考":
+        data["previous_event"] == "第一次排行程表"
+        outputList.append('必然事件:舞會')
+        outputList.append('第一次排行程表')
+    if data["time"] == "大一下"  and data["previous_event"] == "第一次排行程表":
+        data["previous_event"] == "第二次排行程表"
+        outputList.append('第二次排行程表')
+    if data["time"] == "大一下"  and data["previous_event"] == "第二次排行程表":
+        data["previous_event"] == "期末考"
+        outputList.append('期末考')
+        
+    if data["time"] == "大二上"  and data["previous_event"] == "期末考":
+        data["previous_event"] == "第一次排行程表"
+        outputList.append('第一次排行程表')
+    if data["time"] == "大二上"  and data["previous_event"] == "第一次排行程表":
+        data["previous_event"] == "第二次排行程表"
+        outputList.append('第二次排行程表')
+    if data["time"] == "大二上"  and data["previous_event"] == "第二次排行程表":
+        data["previous_event"] == "期中考"
+        outputList.append('期中考')
+    if data["time"] == "大二上"  and data["previous_event"] == "期中考":
+        data["previous_event"] == "第一次排行程表"
+        outputList.append('必然事件:比賽')
+    if data["time"] == "大二上"  and data["previous_event"] == "第一次排行程表":
+        data["previous_event"] == "第二次排行程表"
+        outputList.append('第二次排行程表')
+    if data["time"] == "大二上"  and data["previous_event"] == "第二次排行程表":
+        data["previous_event"] == "期末考"
+        outputList.append('期末考')
+
+    if data["time"] == "大二下"  and data["previous_event"] == "期末考":
+        data["previous_event"] == "第一次排行程表"
+        outputList.append('第一次排行程表')
+    if data["time"] == "大二下"  and data["previous_event"] == "第一次排行程表":
+        data["previous_event"] == "第二次排行程表"
+        outputList.append('第二次排行程表')
+    if data["time"] == "大二下"  and data["previous_event"] == "第二次排行程表":
+        data["previous_event"] == "期中考"
+        outputList.append('期中考')
+    if data["time"] == "大二下"  and data["previous_event"] == "期中考":
+        data["previous_event"] == "第一次排行程表"
+        outputList.append('第一次排行程表')
+    if data["time"] == "大二下"  and data["previous_event"] == "第一次排行程表":
+        data["previous_event"] == "第二次排行程表"
+        outputList.append('第二次排行程表')
+        outputList.append('必然事件:聯誼')
+    if data["time"] == "大二下"  and data["previous_event"] == "第二次排行程表":
+        data["previous_event"] == "期末考"
+        outputList.append('期末考')
+
+    if data["time"] == "大三上"  and data["previous_event"] == "期末考":
+        data["previous_event"] == "第一次排行程表"
+        outputList.append('第一次排行程表')
+    if data["time"] == "大三上"  and data["previous_event"] == "第一次排行程表":
+        data["previous_event"] == "第二次排行程表"
+        outputList.append('第二次排行程表')
+    if data["time"] == "大三上"  and data["previous_event"] == "第二次排行程表":
+        data["previous_event"] == "期中考"
+    if data["time"] == "大三上"  and data["previous_event"] == "期中考":
+        data["previous_event"] == "第一次排行程表"
+        outputList.append('第一次排行程表')
+    if data["time"] == "大三上"  and data["previous_event"] == "第一次排行程表":
+        data["previous_event"] == "第二次排行程表"
+        outputList.append('第二次排行程表')
+    if data["time"] == "大三上"  and data["previous_event"] == "第二次排行程表":
+        data["previous_event"] == "期末考"
+        outputList.append('必然事件:實習')
+        outputList.append('期末考')
+
+    if data["time"] == "大三下"  and data["previous_event"] == "期末考":
+        data["previous_event"] == "第一次排行程表"
+        outputList.append('第一次排行程表')
+    if data["time"] == "大三下"  and data["previous_event"] == "第一次排行程表":
+        data["previous_event"] == "第二次排行程表"
+        outputList.append('必然事件:打疫苗')
+        outputList.append('第二次排行程表')
+    if data["time"] == "大三下"  and data["previous_event"] == "第二次排行程表":
+        data["previous_event"] == "期中考"
+        outputList.append('期中考')
+    if data["time"] == "大三下"  and data["previous_event"] == "期中考":
+        data["previous_event"] == "第一次排行程表"
+        outputList.append('第一次排行程表')
+    if data["time"] == "大三下"  and data["previous_event"] == "第一次排行程表":
+        data["previous_event"] == "第二次排行程表"
+        outputList.append('第二次排行程表')
+    if data["time"] == "大三下"  and data["previous_event"] == "第二次排行程表":
+        data["previous_event"] == "期末考"
+        outputList.append('期末考')
+
+    if data["time"] == "大四上"  and data["previous_event"] == "期末考":
+        data["previous_event"] == "第一次排行程表"
+        outputList.append('第一次排行程表')
+    if data["time"] == "大四上"  and data["previous_event"] == "第一次排行程表":
+        data["previous_event"] == "第二次排行程表"
+        outputList.append('第二次排行程表')
+    if data["time"] == "大四上"  and data["previous_event"] == "第二次排行程表":
+        data["previous_event"] == "期中考"
+        outputList.append('期中考')
+    if data["time"] == "大四上"  and data["previous_event"] == "期中考":
+        data["previous_event"] == "第一次排行程表"
+        outputList.append('第一次排行程表')
+    if data["time"] == "大四上"  and data["previous_event"] == "第一次排行程表":
+        data["previous_event"] == "第二次排行程表"
+        outputList.append('第二次排行程表')
+    if data["time"] == "大四上"  and data["previous_event"] == "第二次排行程表":
+        data["previous_event"] == "期末考"
+        outputList.append('期末考')
+
+    if data["time"] == "大四下"  and data["previous_event"] == "期末考":
+        data["previous_event"] == "第一次排行程表"
+        outputList.append('第一次排行程表')
+    if data["time"] == "大四下"  and data["previous_event"] == "第一次排行程表":
+        data["previous_event"] == "第二次排行程表"
+        outputList.append('第二次排行程表')
+    if data["time"] == "大四下"  and data["previous_event"] == "第二次排行程表":
+        data["previous_event"] == "期中考"
+        outputList.append('期中考')
+    if data["time"] == "大四下"  and data["previous_event"] == "期中考":
+        data["previous_event"] == "第一次排行程表"
+        outputList.append('第一次排行程表')
+    if data["time"] == "大四下"  and data["previous_event"] == "第一次排行程表":
+        data["previous_event"] == "第二次排行程表"
+        outputList.append('第二次排行程表')
+    if data["time"] == "大四下"  and data["previous_event"] == "第二次排行程表":
+        data["previous_event"] == "期末考"
+        outputList.append('期末考')
+        
+    print(outputList)
+    process_event(data, outputList)  # 一旦觸發中途結束，將直接結束遊戲
