d51121e937b4ef6cb9ec9b92c047cf5f781b3a4d
DukeSCSekuD <96483321+DukeSCSekuD@users.noreply.github.com>
2022-01-03 01:16:10 +0800

    Update 結算評分值圖片產生.py

--- function/結算評分值圖片產生.py
+++ function/結算評分值圖片產生.py
@@ -11,16 +11,20 @@ path = os.getcwd()
 title_f = fm.FontProperties(fname=f"{path}/TaipeiSansTCBeta-Regular.ttf", size = 20)
 f = fm.FontProperties(fname=f"{path}/TaipeiSansTCBeta-Regular.ttf", size = 12)
 
+point_table = ['愛情進度', '學業成績', '養生', '聲望']
 
-
+def addlabels(x,y):
+    for i in range(len(x)):
+        plt.text(i, y[i], y[i], ha = 'center')
 
 def score_illu(love_progress, grade, yang_sheng, prestige):
-
+    global point_table
     
-    point_table = ['愛情進度', '學業成績', '養生', '聲望']
+
     points = [love_progress, grade, yang_sheng, prestige]  # 應該要讀入結尾數值
     # 圖形長與寬可以再討論
     x = np.arange(len(point_table))
+    addlabels(point_table, points)
     plt.bar(x, points, color=['red', 'green', 'blue', 'yellow'])
     plt.xticks(x, point_table)
     plt.xlabel('評分值')
