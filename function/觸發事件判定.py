4f5a34df951e21b2cff176d81d80c3258e707ec1
DukeSCSekuD <96483321+DukeSCSekuD@users.noreply.github.com>
2022-01-02 22:48:52 +0800

    Update 觸發事件判定.py

--- function/觸發事件判定.py
+++ function/觸發事件判定.py
@@ -4,12 +4,56 @@ import function.show_event as show
 status = "" 
 
 # 觸發事件的判斷，每學期有些事件會更新一次，讓事件不會重複發生
-study_freq, res_freq, acad_freq, sex_freq, OldExam_freq, First_Date_freq,\
-Sugar_freq, preg_freq, Bike_tow_freq, Vt_freq = True, True, True, True, True, True, True, True, True, True
-def reset():
+study_freq, res_freq, acad_freq, sex_freq, OldExam_freq, First_Date_freq, To_New_World_freq, Too_Stupid_freq,\
+Sugar_freq, preg_freq, Bike_tow_freq, Vt_freq = True, True, True, True, True, True, True, True, True, True, True, True
+def reset():  # 重置事件頻率
     study_freq, res_freq, acad_freq, sex_freq, OldExam_freq,\
-    Sugar_freq, Bike_tow_freq = True, True, True, True, True, True, True
+    Sugar_freq, Bike_tow_freq, To_New_World_freq, Too_Stupid_freq = True, True, True, True, True, True, True, True, True
 
+    
+def blow_wind():  # 樓頂吹風
+    global status
+    if status.social<= 0 or status.wisdom <= 0 or status.fitness <= 0 or status.charm <= 0:
+        return True
+
+
+def To_New_World():  # 轉生異世界
+    global status
+    prob = random.randrange(1, 101)
+    if status.luck < 5 and prob < 2 and To_New_World_freq:
+        return True
+    elif status.luck < 5 and prob > 1 and To_New_World_freq:  # 一學期只會判定一次
+        To_New_World_freq = False
+
+
+def Me_First():  # 明明是我先來的
+    global status
+    if status.charm > 90 and love_progress < 40:
+        return True
+
+
+def Too_Stupid():  # 被二一
+    global pass_or_not, status
+    if pass_or_not:  # 暫定
+        return True
+
+
+def Broke():  # 破產
+    global status
+    if status.Money <= -50000:
+        return True
+
+
+def Wealth_Freedom():  # 財富自由
+    global status
+    if status.Money >= 1000000:
+        return True
+
+
+def IntoDust():  # 火化
+    global status
+    if status.health <= 0:
+        return True
 
 def Vtuber_or_Not():  # 推坑Vt
     global status, Vt_freq
@@ -98,13 +142,27 @@ def check_event(data):
     global status
     status = data["status"]
 
-    outputList = []    
-    TriIncident_List = ['觸發事件:推坑Vt', '觸發事件:參加讀書會', '觸發事件:參加研究專案', '觸發事件:翹課打ㄆ', '觸發事件:獲得考古題', '觸發事件:第一次約會', '觸發事件:婚姻抉擇', '觸發事件:不想努力了', '觸發事件:懷孕', '觸發事件:腳踏車被拖吊']
-    Yes_or_Not_List = [Vtuber_or_Not(), Study_or_not(), Research_or_not(), Sex_or_not(), OldExam_or_not(), First_Date(), Marriage_or_not(), SugarDaddy(), Pregnant(), Bike_tow()]
+    outputList01 = []
+    outputList02 = []
+    TriIncident_List = ['觸發事件:推坑Vt', '觸發事件:參加讀書會', '觸發事件:參加研究專案', '觸發事件:翹課打ㄆ', '觸發事件:獲得考古題', '觸發事件:第一次約會', 
+                        '觸發事件:婚姻抉擇', '觸發事件:不想努力了', '觸發事件:懷孕', '觸發事件:腳踏車被拖吊']
+    Incident_List = ['中途結束事件：樓頂吹風', '中途結束事件：轉生異世界', '中途結束事件：明明是我先來的', '中途結束事件：被二一', '中途結束事件：破產', 
+                     '中途結束事件：財富自由', '中途結束事件：火化']
+    Yes_or_Not = [Vtuber_or_Not(), Study_or_not(), Research_or_not(), Sex_or_not(), OldExam_or_not(), First_Date(), Marriage_or_not(), SugarDaddy(), Pregnant(), Bike_tow()]
+    Mid_End = [blow_wind(), To_New_World(), Me_First(), Too_Stupid(), Broke(), Wealth_Freedom(), IntoDust()]
+    for i in range(7):
+        if Mid_End[i] = True:
+            outputList01.append(Incident_List[i])
+    if len(outputList01) > 1:
+        prob = random.randrange(0, len(outputList01))  # 若中途結束事件發生2種以上，隨機挑選其中一個
+        outputList01 = outputList01[prob]
+    
     for i in range(10):
         if Yes_or_Not_List[i] == True:
-            outputList.append(TriIncident_List[i])
-
+            outputList02.append(TriIncident_List[i])
+    
+    outputList = outputList01 + outputList02
+    
     if data["time"] == "大一上"  and data["previous_event"] == "第一次排行程表":
         data["previous_event"] = "第二次排行程表"
         outputList.append('必然事件:健康檢查')
