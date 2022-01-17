3097b8bdaa1aaaaec92c127694b28661ca2c570f
BloggerWang <bloggerwang1217@protonmail.com>
2022-01-17 22:03:35 +0800

    先生成視窗再import函式

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import system.beginning as bg
 import tkinter as tk
-from fontTools.ttLib import TTFont
 
 window = tk.Tk()
 window.geometry("1280x720")
 window.resizable(0, 0)
 window.title("為美好的台大生活獻上祝福")
+
+import system.beginning as bg
 
 data = {}
 bg.start_game(window, data)
