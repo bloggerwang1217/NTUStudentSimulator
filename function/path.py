bcea0025869094be6453818c81627c35776a3112
BloggerWang <bloggerwang1217@protonmail.com>
2022-01-18 15:17:17 +0800

    test path function

---
+++ function/path.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+def path_transformation(path):
+    file_path = Path(path)
+
+    return file_path
--- function/sound_effect.py
+++ function/sound_effect.py
@@ -1,24 +1,25 @@
 import pygame
 from pygame import mixer
+from function.path import path_transformation as path
 
 pygame.mixer.init()
 
-click_sound = pygame.mixer.Sound("sound_effect/click_sound.mp3")
-enter_game_sound = pygame.mixer.Sound("sound_effect/enter_game_sound.mp3")
-get_letter_sound = pygame.mixer.Sound("sound_effect/get_letter_sound.mp3")
+click_sound = pygame.mixer.Sound(path("sound_effect/click_sound.mp3"))
+enter_game_sound = pygame.mixer.Sound(path("sound_effect/enter_game_sound.mp3"))
+get_letter_sound = pygame.mixer.Sound(path("sound_effect/get_letter_sound.mp3"))
 
 def play_background_music(filename):
-    pygame.mixer.music.load((f"sound_effect/{filename}.mp3"))
+    pygame.mixer.music.load(path(f"sound_effect/{filename}.mp3"))
     pygame.mixer.music.play()
 
 
 def play_event_background_music(name1, name2):
-    pygame.mixer.music.load((f"sound_effect/{name1}/{name2}.mp3"))
+    pygame.mixer.music.load(path(f"sound_effect/{name1}/{name2}.mp3"))
     pygame.mixer.music.play()
 
 
 def play_achievement_music(filename):
-    pygame.mixer.music.load((f"sound_effect/achievement/{filename}.mp3"))
+    pygame.mixer.music.load(path(f"sound_effect/achievement/{filename}.mp3"))
     pygame.mixer.music.play()
 
 
